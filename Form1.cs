3acfb6ae0abc0ba40087e8e52ae3f2cebcf33f4a
Vulfonk <podkalyuk-dima@bk.ru>
2020-11-18 13:48:35 +0500

    Пишу хранилище

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Numerics;
 
 namespace OOP_4
 {
@@ -15,6 +16,56 @@ namespace OOP_4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VectorShape a = new VectorShape(4);
+            Shape s = new Shape();
+            s.setX(4);
+            a[2] = s;
+            label1.Text = a[2].getX().ToString();
+        }
+    }
+
+    class Shape
+    {
+        private int _x;
+        public void setX(int x)
+        {
+            _x = x;
+        }
+        public int getX()
+        {
+            return _x;
+        }
+    }
+
+    class VectorShape
+    {
+        private Shape[] _shapes;
+        private int _size;
+        private int _k;
+        public VectorShape(int size)
+        {
+            _k = 0;
+            _size = size;
+            _shapes = new Shape[_size];
+        }
+        public void resize(int newSize)
+        {
+
+        }
+        public Shape this[int i]
+        {
+            set
+            {
+                _shapes[i] = value;
+            }
+            get
+            {
+                return _shapes[i];
+            }
         }
     }
 }
